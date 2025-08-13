2c94fc46511ce1f0cd93c09280563d2a963494a9
Algodzilla <138765493+alfabell@users.noreply.github.com>
2025-08-13 15:17:18 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return(
+    <>
     <h1 className="text-3xl font-bold underline flex text-center justify-center items-center">abellll</h1>
+    <h2>abel kedua</h2>
+    </>
   )
 }
